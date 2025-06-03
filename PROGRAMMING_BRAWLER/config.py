1b890abefa47927a1614f585638809236e15723b
MateiEnache <146372728+MateiEnache@users.noreply.github.com>
2025-06-02 21:41:17 -0700

    Update config.py

--- PROGRAMMING_BRAWLER/config.py
+++ PROGRAMMING_BRAWLER/config.py
@@ -1,1 +1,40 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+TILESIZE = 32
+FPS = 60
 
+OTHER_LAYER = 5
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+ENEMY_SPEED = 2
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (16,109,156)
+WHITE = (255, 255, 255)
+
+tilemap = [
+        '        XXXXX               ',
+        '     XXXDDDDDXXX            ',
+        '    XDDD....,DDXX          ',
+        '  XXD..R,,;...,DDXX        ',
+        ' XXD..,,,,..r.,..DDX       ',
+        ' XD..;,?,...,..R..,DX     ',
+        'XD..,.r....?,.,....,DX    ',
+        'XD..,,...,;.,?,..R...DX   ',
+        'XD.,..?,.R.;,,..,....DDX  ',
+        'XD....,;;.?,P;,E.?;...,DX ',
+        'XD..;r...,.r,...r.?.,..DX ',
+        ' XD...,?.,,?...,R...,.;DX ',
+        ' XD...,....,..,,.;.,..DDX ',
+        '  XD...;R..,;..r..,..DDX  ',
+        '  XDD...?,;,.?.,..,DDDX   ',
+        '   XDD..,..R....,DDDXX     ',
+        '    XXD.....;..,DDXX       ',
+        '     XXDDDBBBDDDXX         ',
+        '      XXXXXXXXX          ',
+]
